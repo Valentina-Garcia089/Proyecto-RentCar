0671089e406a1105fd697270d93534f0ca49fc0a
crobayo1829 <carolrobayo18@gmail.com>
2025-06-01 01:30:00 -0500

    actualizar usuarios

--- src/main/java/org/puj/proyectorentcar/Negocio/Cliente.java
+++ src/main/java/org/puj/proyectorentcar/Negocio/Cliente.java
@@ -1,4 +1,7 @@
 package org.puj.proyectorentcar.Negocio;
 
 public class Cliente extends Usuario{
+    public Cliente(String nombre, String apellido, int edad, String direccion, long telefono, String correo, String tipoId, long numeroId, String contrasenia) {
+        super(nombre, apellido, edad, direccion, telefono, correo, tipoId, numeroId, contrasenia);
+    }
 }
--- src/main/java/org/puj/proyectorentcar/Negocio/Usuario.java
+++ src/main/java/org/puj/proyectorentcar/Negocio/Usuario.java
@@ -1,4 +1,101 @@
 package org.puj.proyectorentcar.Negocio;
 
 public class Usuario {
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String direccion;
+    private long telefono;
+    private String correo;
+    private String tipoId;
+    private long numeroId;
+    private String contrasenia;
+
+        // Constructor y getters/setters
+
+
+    public Usuario(String nombre, String apellido, int edad, String direccion, long telefono, String correo, String tipoId, long numeroId, String contrasenia) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.tipoId = tipoId;
+        this.numeroId = numeroId;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public long getNumeroId() {
+        return numeroId;
+    }
+
+    public void setNumeroId(long numeroId) {
+        this.numeroId = numeroId;
+    }
+
+    public String getTipoId() {
+        return tipoId;
+    }
+
+    public void setTipoId(String tipoId) {
+        this.tipoId = tipoId;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
